b4d5ebbd656f951d40fd1652eaa5263dd3aac0b7
Daniel tu papi <daniel.loaizan@udea.edu.co>
2023-10-10 00:04:03 -0500

    final

--- components/Buttonhireme.tsx
+++ components/Buttonhireme.tsx
@@ -1,11 +1,8 @@
-interface ButtonhiremeProps{
-    handleClick:()=>void;
 
-}
-const Buttonhireme = ({handleClick=()=>{}}:ButtonhiremeProps) => {
+const Buttonhireme = () => {
     return (
         
-      <button onClick={handleClick} className=" bg-orange-500 hover:scale-105  text-left w-24 rounded-md" >
+      <button  className=" bg-orange-500 hover:scale-105  text-left w-24 rounded-md" >
         
           <span className="bg-orange-500 font-semibold text-right h-full">Sobre Mí</span> 
       </button>
